1d33059bbc586a49ab0c6bc62fbd32eeba0f67b4
Muskan <muskangupta9566@gmail.com>
2024-06-11 16:22:44 +0530

    add code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import XDictionary from "./dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <XDictionary/>
+    </div>
+  );
+}
+
+export default App;
